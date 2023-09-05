6e0150832f93d9f861331c5d6b1286c780605f42
Chris Mastris <cmastris@hotmail.com>
2023-09-05 20:23:43 +0100

    Configure server logging

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const logging = require('morgan');
 
 const api = express();
 const port = process.env.PORT;
+
+// https://expressjs.com/en/resources/middleware/morgan.html
+api.use(logging(process.env.LOGGING));
 
 api.get('/', (req, res) => {
   res.send('Hello World!');
