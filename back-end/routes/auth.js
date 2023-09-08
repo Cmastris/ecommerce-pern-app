d53a31cd8118e87651c780f496feda6ccd25477f
Chris Mastris <cmastris@hotmail.com>
2023-09-08 15:48:15 +0100

    Add POST /login route

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -38,5 +38,27 @@ router.post('/register', jsonParser, async (req, res) => {
   }
 });
 
+router.post('/login', jsonParser, async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await db.getUserByUsername(username);
+    if (!user) {
+      return res.status(404).send(
+        `Login failed. A user with the username '${username}' does not exist.`
+      );
+    }
+    const matchedPassword = await bcrypt.compare(password, user.hashed_pw);
+    if (!matchedPassword) {
+      return res.status(401).send(`Login failed. Username or password is incorrect.`);
+    } else {
+      // TODO: implement authentication using Passport local
+      res.status(200).json({ id: user.id, username: user.username });
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500).send('Login failed. Please ensure you are providing the required data.');
+  }
+});
 
 module.exports = router;
