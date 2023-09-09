aadeffd6513671c2e4a8263bef2ae2ac94aa0633
Chris Mastris <cmastris@hotmail.com>
2023-09-09 11:51:19 +0100

    Add POST /register tests

--- back-end/routes/auth.test.js
+++ back-end/routes/auth.test.js
@@ -1,11 +1,35 @@
 const request = require('supertest');
 const api = require('../index');
 
+// https://jestjs.io/docs/manual-mocks
+jest.mock('../db/index');
+
 // https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
 
 test('GET /auth/register returns a 404 status code', async () => {
    const res = await request(api).get('/auth/register');
    expect(res.statusCode).toBe(404);
+});
+
+test('POST /auth/register with missing data returns a 500 status code', async () => {
+  const res = await request(api).post('/auth/register').send({});
+  expect(res.statusCode).toBe(500);
+});
+
+test('POST /auth/register with an existing username returns a 400 status code', async () => {
+  const res = await request(api).post('/auth/register').send(
+    { username: "usernameExists", password: "pw" }
+  );
+  expect(res.statusCode).toBe(400);
+});
+
+test('POST /auth/register with a new username returns a success response', async () => {
+  const res = await request(api).post('/auth/register').send(
+    { username: "newUsername", password: "pw" }
+  );
+
+  expect(res.statusCode).toBe(201);
+  expect(res.body).toStrictEqual({ id: 1, username: "newUsername" });
 });
 
 test('GET /auth/login returns a 404 status code', async () => {
