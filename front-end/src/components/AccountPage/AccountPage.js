b5b0d3223deff77bc290dcee93e22f157c2565a7
Chris Mastris <cmastris@hotmail.com>
2023-09-24 16:39:17 +0100

    Add AccountPage summary content

--- front-end/src/components/AccountPage/AccountPage.js
+++ front-end/src/components/AccountPage/AccountPage.js
@@ -1,7 +1,17 @@
+import { Link, useRouteLoaderData } from "react-router-dom";
+
 export default function AccountPage() {
+  // https://reactrouter.com/en/main/hooks/use-route-loader-data
+  const authData = useRouteLoaderData("app");
+
   return (
     <div>
       <h1>Your account</h1>
+      {!authData.logged_in ?
+      <p>Please <Link to="/login">log in</Link> to view your account details and orders.</p>
+      :
+      <p>You are logged in as {authData.email_address}.</p>
+      }
     </div>
   );
 }
