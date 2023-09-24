d4f33966d5234e59b45ab2656358eb0e98947516
Chris Mastris <cmastris@hotmail.com>
2023-09-24 20:39:20 +0100

    Implement log out functionality

--- front-end/src/components/MainNav/MainNav.js
+++ front-end/src/components/MainNav/MainNav.js
@@ -1,8 +1,29 @@
-import { NavLink, useRouteLoaderData } from "react-router-dom";
+import { NavLink, useNavigate, useRouteLoaderData } from "react-router-dom";
 
 export default function MainNav() {
   // https://reactrouter.com/en/main/hooks/use-route-loader-data
   const authData = useRouteLoaderData("app");
+  const navigate = useNavigate();
+
+  async function handleClickLogOut () {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/auth/logout`,
+        {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          credentials: 'include',
+        }
+      );
+      if (!res.ok) {
+        throw new Error('Unexpected status code.');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      navigate(0);  // Refresh page to clear auth state and re-render
+    }
+  }
 
   return (
     <nav>
@@ -16,7 +37,7 @@ export default function MainNav() {
       <ul>
         <li><NavLink to="/account">Account</NavLink></li>
         <li><NavLink to="/cart">Cart</NavLink></li>
-        <li><NavLink to="#" onClick={() => console.log('log out')}>Log Out</NavLink></li>
+        <li><NavLink to="#" onClick={handleClickLogOut}>Log Out</NavLink></li>
       </ul>
       :
       <ul>
