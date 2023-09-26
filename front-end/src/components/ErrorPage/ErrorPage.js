99e1ac309a80118167d9c6be67bbfc2676b9e34d
Chris Mastris <cmastris@hotmail.com>
2023-09-26 20:51:20 +0100

    Render more specific ErrorPage content if 404

--- front-end/src/components/ErrorPage/ErrorPage.js
+++ front-end/src/components/ErrorPage/ErrorPage.js
@@ -2,12 +2,13 @@ import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
+  const is404 = error.status === 404;
   console.error(error);
 
   return (
     <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>{is404 ? '404 (Not Found)' : 'Oops!'}</h1>
+      <p>{is404 ? 'Sorry, this page does not exist.' : 'Sorry, an unexpected error has occurred.'}</p>
       <p>
         <em>{error.statusText || error.message}</em>
       </p>
