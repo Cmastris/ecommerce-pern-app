1e1203f57608909ca71c419d6c9a2ccd647819ce
Chris Mastris <cmastris@hotmail.com>
2023-09-29 18:48:12 +0100

    Add Cart summary content

--- front-end/src/features/cart/Cart.js
+++ front-end/src/features/cart/Cart.js
@@ -1,7 +1,17 @@
+import { Link, useRouteLoaderData } from "react-router-dom";
+
 export function Cart() {
+  // https://reactrouter.com/en/main/hooks/use-route-loader-data
+  const authData = useRouteLoaderData("app");
+
   return (
     <div>
       <h1>Cart</h1>
+      {!authData.logged_in ?
+      <p>Please <Link to="/login?redirect=/cart">log in</Link> to view your cart.</p>
+      :
+      <p>You are logged in as {authData.email_address}. Your cart items are listed below.</p>
+      }
     </div>
   );
 }
