4feda2c681be729535cdbde5d4a2cb9b33a12cbd
Chris Mastris <cmastris@hotmail.com>
2023-09-29 10:54:11 +0100

    Add a ProductDetail loader to fetch product data

--- front-end/src/features/products/ProductDetail.js
+++ front-end/src/features/products/ProductDetail.js
@@ -1,7 +1,37 @@
+import { useLoaderData } from "react-router-dom";
+
+
+export async function productDetailLoader({ params }) {
+  // https://reactrouter.com/en/main/start/tutorial#loading-data
+  // https://reactrouter.com/en/main/route/loader
+  try {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/products/${params.id}`);
+    
+    if (res.status === 404) {
+      // https://reactrouter.com/en/main/route/error-element#throwing-manually
+      throw new Response('Not Found', { status: 404 });
+    } else if (!res.ok) {
+      throw new Error('Unsuccessful product fetch.');
+    }
+
+    const productData = await res.json();
+    return { productData };
+
+  } catch (error) {
+    if (error.status === 404) {
+      throw error;  // Serve 404 error page
+    }
+    return { error: 'Sorry, product data could not be loaded.' };
+  }
+}
+
+
 export function ProductDetail() {
+  const { productData, error } = useLoaderData();
+
   return (
     <div>
-      <h1>Product Detail</h1>
+      <h1>{error ? error : productData.name }</h1>
     </div>
   );
 }
--- front-end/src/routing.js
+++ front-end/src/routing.js
@@ -4,7 +4,7 @@ import AccountPage from "./components/AccountPage/AccountPage";
 import { App, authLoader } from "./App/App";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { LoginPage, loginAction } from "./features/auth/LoginPage";
-import { ProductDetail } from "./features/products/ProductDetail";
+import { ProductDetail, productDetailLoader } from "./features/products/ProductDetail";
 import { ProductFeed, productFeedLoader } from "./features/products/ProductFeed";
 import { RegistrationPage, registerAction } from "./features/auth/RegistrationPage";
 
@@ -45,6 +45,7 @@ export const router = createBrowserRouter([
       {
         path: "products/:id/:productNameSlug",
         element: <ProductDetail />,
+        loader: productDetailLoader,
       },
     ],
   },
