a13fabc9b8c4ca485232f79db6360aa139f7c4c4
Chris Mastris <cmastris@hotmail.com>
2023-09-30 11:37:21 +0100

    Render CartItem content

--- front-end/src/features/cart/CartItem.js
+++ front-end/src/features/cart/CartItem.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import { getProductDetailPath } from "../products/utils";
+
 export default function CartItem({ productData }) {
+  const { product_id, product_name, product_price } = productData;
+  const productPath = getProductDetailPath(product_id, product_name);
+
   return (
     <article>
-      <h3>{productData.product_name}</h3>
+      <hr />
+      <div>
+        <h3><Link to={productPath}>{product_name}</Link></h3>
+        <p>{product_price}</p>
+        {/* TODO: implement delete functionality */}
+        <button>Remove</button>
+      </div>
     </article>
   );
 }
