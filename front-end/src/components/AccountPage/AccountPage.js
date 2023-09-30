63bb678cfc798b99506fd844c39a2dc833bdfcc2
Chris Mastris <cmastris@hotmail.com>
2023-09-30 13:50:45 +0100

    Add cart link to account page

--- front-end/src/components/AccountPage/AccountPage.js
+++ front-end/src/components/AccountPage/AccountPage.js
@@ -4,14 +4,20 @@ export default function AccountPage() {
   // https://reactrouter.com/en/main/hooks/use-route-loader-data
   const authData = useRouteLoaderData("app");
 
+  if (!authData.logged_in) {
+    return (
+      <div>
+        <h1>Your account</h1>
+        <p>Please <Link to="/login">log in</Link> to view your account details and orders.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Your account</h1>
-      {!authData.logged_in ?
-      <p>Please <Link to="/login">log in</Link> to view your account details and orders.</p>
-      :
       <p>You are logged in as {authData.email_address}.</p>
-      }
+      <Link to="/cart">Go to cart</Link>
     </div>
   );
 }
