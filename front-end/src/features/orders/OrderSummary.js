095879aa0dfd02b65cdfd90c810b8d43dc3d8de7
Chris Mastris <cmastris@hotmail.com>
2023-10-03 19:56:41 +0100

    Populate OrderSummary content

--- front-end/src/features/orders/OrderSummary.js
+++ front-end/src/features/orders/OrderSummary.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import { getDateTimeString } from "./utils";
+
 export default function OrderSummary({ orderData }) {
+  const { order_id, order_placed_time, order_status, total_cost } = orderData;
+  const formattedTime = getDateTimeString(order_placed_time);
+
   return (
     <article>
-      <h3>Order</h3>
+      <hr />
+      <h3><Link to={`/orders/${order_id}`}>Order #{order_id} ({order_status})</Link></h3>
+      <p>{formattedTime}</p>
+      <p>{total_cost}</p>
     </article>
   );
 }
