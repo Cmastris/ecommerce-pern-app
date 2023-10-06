b2eb2f5b1626c618b4181afeb9a9676b75c4a3d6
Chris Mastris <cmastris@hotmail.com>
2023-10-06 17:32:05 +0100

    Style FallbackErrorPage

--- front-end/src/components/FallbackErrorPage/FallbackErrorPage.js
+++ front-end/src/components/FallbackErrorPage/FallbackErrorPage.js
@@ -1,5 +1,7 @@
 import { Link, useRouteError } from "react-router-dom";
+
 import Header from "../Header/Header";
+import utilStyles from "../../App/utilStyles.module.css";
 
 
 export default function FallbackErrorPage() {
@@ -9,17 +11,17 @@ export default function FallbackErrorPage() {
   console.error(error);
 
   return (
-    <div>
+    <>
       {is404 ? <Header /> : null}
-      <h1>{is404 ? "404 (Not Found)" : "Oops!"}</h1>
-      <p>{is404 ? "Sorry, this page does not exist." : "Sorry, an unexpected error has occurred."}</p>
-      <p>
-        <em>{error.statusText || error.message}</em>
-      </p>
-      <hr></hr>
-      <h3>
-        <Link to="/">Go to the homepage</Link>
-      </h3>
-    </div>
+      <main className={utilStyles.pageXPadding}>
+        <h1 className={utilStyles.h1}>{is404 ? "404 (Not Found)" : "Oops!"}</h1>
+        <p>{is404 ? "Sorry, this page does not exist." : "Sorry, an unexpected error has occurred."}</p>
+        <p>
+          <em>{error.statusText || error.message}</em>
+        </p>
+        <hr className={utilStyles.separator}></hr>
+        <Link to="/" className={utilStyles.buttonLink}>Visit the homepage</Link>
+      </main>
+    </>
   );
 }
