8958747049e2d5d92b8f5d93853f49c5fd6d17a6
Chris Mastris <cmastris@hotmail.com>
2023-10-07 14:47:07 +0100

    Style InlineErrorPage

--- front-end/src/components/InlineErrorPage/InlineErrorPage.js
+++ front-end/src/components/InlineErrorPage/InlineErrorPage.js
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
+import utilStyles from "../../App/utilStyles.module.css";
+
 
 export default function InlineErrorPage({ pageName, type, message, loginRedirect }) {
 
   function renderMessage() {
     if (type === "login_required") {
       const path = loginRedirect ? `/login?redirect=${loginRedirect}` : "/login";
-      return <p>Please <Link to={path}>log in</Link> to view this page.</p>;
+      const link = <Link to={path} className={utilStyles.link}>log in</Link>;
+      return <p>Please {link} to view this page.</p>;
     } else {
       return <p>{message}</p>;
     }
   }
 
   return (
-    <div>
-      <h1>{pageName}</h1>
-      {renderMessage()}
+    <div className={utilStyles.pageXPadding}>
+      <h1 className={utilStyles.h1}>{pageName}</h1>
+      <div className={utilStyles.largeText}>
+        {renderMessage()}
+      </div>
     </div>
   );
 }
