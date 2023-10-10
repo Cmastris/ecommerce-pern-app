f208ad20db7cef60cfe8200eb07025faa0da2ce5
Chris Mastris <cmastris@hotmail.com>
2023-10-10 19:30:31 +0100

    Style empty cart message

--- front-end/src/features/orders/utils.js
+++ front-end/src/features/orders/utils.js
@@ -1,4 +1,6 @@
 import { OrderItem } from "./OrderItem";
+import utilStyles from "../../App/utilStyles.module.css";
+
 
 export function getDateTimeString(rawString) {
   const n = "numeric";
@@ -6,11 +8,12 @@ export function getDateTimeString(rawString) {
   return new Date(rawString).toLocaleString("en-GB", options);
 }
 
+
 export function renderOrderItems(orderItemsData, editable=true) {
   // Cart items (i.e. pending order items) or completed order items
   const itemsCount = orderItemsData.length;
   if (itemsCount === 0) {
-    return <p>Your cart is empty.</p>;
+    return <p className={utilStyles.emptyFeedMessage}>Your cart is empty.</p>;
   }
   const orderItems = orderItemsData.map((item, index) => {
     if (index + 1 === itemsCount) {
