dd50a7a50389eee48d1f0d035874dd75ffab46ed
Chris Mastris <cmastris@hotmail.com>
2023-10-10 19:27:17 +0100

    Style OrderItem

--- front-end/src/features/orders/OrderItem.js
+++ front-end/src/features/orders/OrderItem.js
@@ -1,5 +1,8 @@
 import { Form, Link } from "react-router-dom";
+
 import { getProductDetailPath } from "../products/utils";
+import styles from "./OrderItem.module.css";
+import utilStyles from "../../App/utilStyles.module.css";
 
 
 export async function removeCartItemAction({ request }) {
@@ -32,20 +35,24 @@ export function OrderItem({ productData, editable, lastItem }) {
   const productPath = getProductDetailPath(product_id, product_name);
 
   return (
-    <article>
-      <hr />
-      <div>
-        <h3><Link to={productPath}>{product_name}</Link></h3>
-        <p>{product_price}</p>
+    <div className={styles.orderItem}>
+      <hr className={utilStyles.separator} />
+      <article className={styles.flexContainer}>
+        <div className={styles.contentContainer}>
+          <strong>
+            <Link to={productPath} className={`${utilStyles.largeText} ${utilStyles.link}`}>{product_name}</Link>
+          </strong>
+          <div className={utilStyles.mt1rem}>{product_price}</div>
+        </div>
         {editable ?
         <Form method="post">
           <input type="hidden" name="product_id" value={product_id}></input>
           <input type="hidden" name="product_name" value={product_name}></input>
-          <button type="submit">Remove</button>
+          <button type="submit" className={styles.button}>Remove</button>
         </Form>
         : null}
-      </div>
-      {lastItem ? <hr /> : null}
-    </article>
+      </article>
+      {lastItem ? <hr className={utilStyles.separator} /> : null}
+    </div>
   );
 }
