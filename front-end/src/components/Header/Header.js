d64320d3f4db7682b91f545e242706243cda0d3e
Chris Mastris <cmastris@hotmail.com>
2023-10-11 22:33:22 +0100

    Add homepage link to Header logo

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 import MainNav from "../MainNav/MainNav";
 import styles from "./Header.module.css";
+
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Pern<span className={styles.orange}>ecom</span></div>
+      <div className={styles.logoContainer}>
+        <Link to="/" className={styles.logo}>Pern<span className={styles.orange}>ecom</span></Link>
+      </div>
       <MainNav />
     </header>
   );
