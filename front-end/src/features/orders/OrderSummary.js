081a997474fa8c134097643e9468c45f1d96dc20
Chris Mastris <cmastris@hotmail.com>
2023-10-11 21:04:06 +0100

    Style OrderSummary

--- front-end/src/features/orders/OrderSummary.js
+++ front-end/src/features/orders/OrderSummary.js
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
+
 import { getDateTimeString } from "./utils";
+import styles from "./OrderSummary.module.css";
+import utilStyles from "../../App/utilStyles.module.css";
+
 
 export default function OrderSummary({ orderData, lastItem }) {
   const { order_id, order_placed_time, order_status, total_cost } = orderData;
   const formattedTime = getDateTimeString(order_placed_time);
+  const orderDetailsPath = `/orders/${order_id}`;
 
   return (
-    <article>
-      <hr />
-      <h3><Link to={`/orders/${order_id}`}>Order #{order_id} ({order_status})</Link></h3>
-      <p>{formattedTime}</p>
-      <p>{total_cost}</p>
-      {lastItem ? <hr /> : null}
-    </article>
+    <div className={styles.orderSummary}>
+      <hr className={utilStyles.separator} />
+      <article className={styles.flexContainer}>
+        <div className={styles.contentContainer}>
+          <strong>
+            <Link to={orderDetailsPath} className={`${utilStyles.largeText} ${utilStyles.link}`}>#{order_id}, {order_status}</Link>
+          </strong>
+          <div className={`${utilStyles.mt1rem} ${utilStyles.smallText} ${utilStyles.bold}`}>{formattedTime}</div>
+          <div className={utilStyles.mt1rem}>{total_cost}</div>
+        </div>
+        <div>
+          <Link to={orderDetailsPath} className={`${utilStyles.button} ${styles.button}`}>View details</Link>
+        </div>
+      </article>
+      {lastItem ? <hr className={utilStyles.separator} /> : null}
+    </div>
   );
 }
