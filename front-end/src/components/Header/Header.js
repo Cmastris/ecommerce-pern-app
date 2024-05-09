3cab88908e6e2d90f6bb77c0c1206da81c9e6792
Chris Mastris <cmastris@hotmail.com>
2024-05-09 17:38:22 +0100

    Render `LowerHeader` within `Header`

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,9 +1,14 @@
+import LowerHeader from "../LowerHeader/LowerHeader";
 import UpperHeader from "../UpperHeader/UpperHeader";
+
+import styles from "./Header.module.css";
+
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <UpperHeader />
+      <LowerHeader />
     </header>
   );
 }
