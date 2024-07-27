8ec76ef2ee4641a511b3ee003dd021f59a07dc26
Chris Mastris <cmastris@hotmail.com>
2024-07-27 14:22:25 +0100

    Covert StarRating to TypeScript

--- front-end/src/components/StarRating/StarRating.tsx
+++ front-end/src/components/StarRating/StarRating.tsx
@@ -2,25 +2,22 @@ import { MdStar, MdStarOutline, MdStarHalf } from "react-icons/md";
 import styles from "./StarRating.module.css";
 
 
-export default function StarRating({ rating }) {
+export default function StarRating({ rating }: { rating: string | number }) {
 
   function renderStars() {
-    const stars = Math.round(rating * 2) / 2;  // Rounded to the nearest .5
-    const wholeStars = Math.floor(stars);  // Rounded down to whole stars
-    const ratingElements = [];
+    const wholeAndHalfStars = Math.round(Number(rating) * 2) / 2;  // Rounded to the nearest .5
+    const wholeStars = Math.floor(wholeAndHalfStars);  // Rounded down to whole stars
+    const ratingElements: React.JSX.Element[] = [];
 
     for (let x=0; x < wholeStars; x++) {
       ratingElements.push(<MdStar key={ratingElements.length} />);
     }
-
-    if (stars % 1 !== 0) {
+    if (wholeAndHalfStars % 1 !== 0) {
       ratingElements.push(<MdStarHalf key={ratingElements.length} />);
     }
-
     while (ratingElements.length < 5) {
       ratingElements.push(<MdStarOutline key={ratingElements.length} />);
     }
-
     return ratingElements;
   }
 
