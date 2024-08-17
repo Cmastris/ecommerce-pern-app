a301e3a76c5da51059f36865a7263f489bec7a87
Chris Mastris <cmastris@hotmail.com>
2024-08-17 13:35:47 +0100

    Convert Cart to TypeScript

--- front-end/src/features/orders/Cart.tsx
+++ front-end/src/features/orders/Cart.tsx
@@ -1,46 +1,57 @@
 import { Link, useActionData, useLoaderData, useRouteLoaderData } from "react-router-dom";
 
+import { AuthData } from "../auth/authData";
+import { OrderItemData } from "./orderItemData";
+import { RemoveCartItemActionData } from "./OrderItem";
 import InlineErrorPage from "../../components/InlineErrorPage/InlineErrorPage";
 import InlineLink from "../../components/InlineLink/InlineLink";
 import { getProductDetailPath } from "../products/utils";
 import { renderOrderItems } from "./utils";
+
 import utilStyles from "../../App/utilStyles.module.css";
+
+
+type LoaderData = {
+  cartData: OrderItemData[],
+  loaderErrMsg?: string
+}
 
 
 export async function cartLoader() {
   // https://reactrouter.com/en/main/start/tutorial#loading-data
   // https://reactrouter.com/en/main/route/loader
+  let cartData: OrderItemData[] = [];
   try {
     const res = await fetch(
       `${process.env.REACT_APP_API_BASE_URL}/cart`,
       { credentials: "include" }
     );
     if (res.ok) {
-      const cartData = await res.json();
+      cartData = await res.json();
       return { cartData };
     }
     throw new Error("Unexpected status code.");
   } catch (error) {
-    return { cartLoaderError: "Sorry, your cart could not be loaded. Please try again later." };
+    return { cartData, loaderErrMsg: "Sorry, your cart could not be loaded. Please try again later." };
   }
 }
 
 
 export function Cart() {
   // https://reactrouter.com/en/main/hooks/use-route-loader-data
-  const authData = useRouteLoaderData("app");
-  const { cartData, cartLoaderError } = useLoaderData();
-  const removalResult = useActionData();
+  const authData = useRouteLoaderData("app") as AuthData;
+  const { cartData, loaderErrMsg } = useLoaderData() as LoaderData;
+  const removalResult = useActionData() as RemoveCartItemActionData;
 
   if (!authData.logged_in) {
     return <InlineErrorPage pageName="Cart" type="login_required" loginRedirect="/cart" />;
-  } else if (cartLoaderError) {
-    return <InlineErrorPage pageName="Cart" message={cartLoaderError} />;
+  } else if (loaderErrMsg) {
+    return <InlineErrorPage pageName="Cart" message={loaderErrMsg} />;
   }
 
   function renderRemovalMessage() {
     const { error, productId, productName } = removalResult;
-    let message;
+    let message: string | React.JSX.Element;
     if (error) {
       message = `Sorry, '${productName}' couldn't be removed from your cart.`;
     } else {
@@ -60,7 +71,7 @@ export function Cart() {
         : null }
       </p>
       {removalResult ? renderRemovalMessage() : null}
-      {renderOrderItems(cartData, cartLoaderError)}
+      {renderOrderItems(cartData, true)}
       {cartData?.length > 0 ?
       <Link to="/checkout" className={utilStyles.button}>Go to checkout</Link>
       : null}
