f2149e6dd258e4ffd160f1d6932056c3f1e634f6
Chris Mastris <cmastris@hotmail.com>
2024-08-17 14:31:04 +0100

    Convert CheckoutPage to TypeScript

--- front-end/src/features/orders/CheckoutPage.tsx
+++ front-end/src/features/orders/CheckoutPage.tsx
@@ -1,11 +1,14 @@
 import { Form, Link, redirect, useActionData, useLoaderData, useRouteLoaderData } from "react-router-dom";
 
+import { AuthData } from "../auth/authData";
+import { CartLoaderData } from "./Cart";
 import { renderOrderItems } from "./utils";
 import InlineErrorPage from "../../components/InlineErrorPage/InlineErrorPage";
+
 import utilStyles from "../../App/utilStyles.module.css";
 
 
-export async function checkoutAction({ request }) {
+export async function checkoutAction({ request }: { request: Request }) {
   // https://reactrouter.com/en/main/start/tutorial#data-writes--html-forms
   // https://reactrouter.com/en/main/route/action
   let formData = await request.formData();
@@ -23,26 +26,27 @@ export async function checkoutAction({ request }) {
     );
 
     if (res.ok) {
-      const { order_id } = await res.json();
+      const { order_id }: { order_id: number } = await res.json();
       return redirect(`/checkout/${order_id}/payment`);
     }
     throw new Error("Unexpected status code.");
   } catch (error) {
-    return { checkoutError: "Sorry, your order could not be completed. Please try again later." };
+    return { checkoutErrMsg: "Sorry, your order could not be completed. Please try again later." };
   }
 }
 
 
 export function CheckoutPage() {
   // https://reactrouter.com/en/main/hooks/use-route-loader-data
-  const authData = useRouteLoaderData("app");
-  const { cartData, cartLoaderError } = useLoaderData();
-  const checkoutError = useActionData()?.checkoutError;
+  const authData = useRouteLoaderData("app") as AuthData;
+  const { cartData, cartLoaderErrMsg } = useLoaderData() as CartLoaderData;
+  const checkoutActionData = useActionData() as { checkoutErrMsg: string } | undefined;
+  const checkoutErrMsg = checkoutActionData?.checkoutErrMsg;
 
   if (!authData.logged_in) {
     return <InlineErrorPage pageName="Checkout" type="login_required" loginRedirect="/cart" />;
-  } else if (cartLoaderError) {
-    return <InlineErrorPage pageName="Checkout" message={cartLoaderError} />;
+  } else if (cartLoaderErrMsg) {
+    return <InlineErrorPage pageName="Checkout" message={cartLoaderErrMsg} />;
   } else if (cartData.length === 0) {
     return <InlineErrorPage pageName="Checkout" message="Your cart is empty so it is not possible to checkout." />;
   }
@@ -67,14 +71,14 @@ export function CheckoutPage() {
       <h2>Delivery address</h2>
       <Form method="post" className={utilStyles.stackedForm}>
         <label htmlFor="address" className={utilStyles.label}>Delivery name and address</label>
-        <textarea id="address" className={utilStyles.input} name="address" rows="5" minLength={15} maxLength={300} required />
+        <textarea id="address" className={utilStyles.input} name="address" rows={6} minLength={15} maxLength={300} required />
         <label htmlFor="postcode" className={utilStyles.label}>Postcode</label>
         <input id="postcode" className={utilStyles.input} type="text" name="postcode" minLength={5} maxLength={8} required />
         <button type="submit" className={`${utilStyles.mt2rem} ${utilStyles.button}`}>Continue to payment</button>
       </Form>
-      {checkoutError ? (
+      {checkoutErrMsg ? (
         <div className={utilStyles.mt2rem}>
-          <p className={`${utilStyles.mb2rem} ${utilStyles.red}`}><strong>{checkoutError}</strong></p>
+          <p className={`${utilStyles.mb2rem} ${utilStyles.red}`}><strong>{checkoutErrMsg}</strong></p>
           <Link to="/" className={utilStyles.button}>Continue shopping</Link>
         </div>
       ) : null}
