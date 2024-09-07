e9b3aa2b8a06044e2b300a0c976dbff4dcdc244b
Chris Mastris <cmastris@hotmail.com>
2024-09-07 13:05:38 +0100

    Convert front-end test files to TypeScript

--- front-end/src/test-setup/testRouters.tsx
+++ front-end/src/test-setup/testRouters.tsx
@@ -1,6 +1,10 @@
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 
-export function createRouterProvider(component, path = "/", initialEntries = ["/"]) {
+export function createRouterProvider(
+  component: React.JSX.Element,
+  path = "/",
+  initialEntries = ["/"]
+) {
   // Create a simple `RouterProvider` for tests that don't involve routing
   // This is required when rendering components that use React Router functions/components
   const router = createMemoryRouter(
